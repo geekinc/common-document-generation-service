a079c9d856d372486b4993a83de7d0f3dc9ae324
Ben Inkster <ben@geekinc.ca>
2023-05-19 11:34:17 -0600

    Add serverless endpoint

---
+++ serverless/app.js
@@ -0,0 +1,26 @@
+const awsServerlessExpress = require('aws-serverless-express');
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/search', async (req, res) => {
+    console.log(req.body);
+    try {
+        const response = await axios.post('https://api.apollo.io/v1/mixed_people/search', req.body);
+        res.send(response.data);
+    } catch (error) {
+        console.error(`Error: ${error}`);
+        res.status(500).send('There was an error processing your request');
+    }
+});
+
+const server = awsServerlessExpress.createServer(app);
+
+exports.handler = (event, context) => {
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+    awsServerlessExpress.proxy(server, event, context);
+};
