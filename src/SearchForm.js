2a8f6f7dc9ac127b602f84898c526cbf51a81372
Ben Inkster <ben@geekinc.ca>
2023-07-28 10:33:39 -0600

    Fix the endpoint to call

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -94,8 +94,8 @@ const SearchForm = () => {
         event.preventDefault();
         try {
             setIsLoading(true);
-            // const response = await fetch('https://ty9vcdxik7.execute-api.us-east-1.amazonaws.com/prod/api/search', {
-            const response = await fetch('http://localhost:3000/dev/api/search', {
+            const response = await fetch('https://ty9vcdxik7.execute-api.us-east-1.amazonaws.com/prod/api/search', {
+            // const response = await fetch('http://localhost:3000/dev/api/search', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(payload)
