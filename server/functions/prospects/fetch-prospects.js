b0419761ff4922c16f64b93ad1ec61f6231a7e21
Ben Inkster <ben@geekinc.ca>
2023-09-11 16:18:42 -0600

    Remove loop - just process one request

--- server/functions/prospects/fetch-prospects.js
+++ server/functions/prospects/fetch-prospects.js
@@ -23,46 +23,34 @@ export async function main(event, context) {
     console.log(event.body);
 
     try {
-        let var_method = 'API';
-        let var_record = [];
+        let var_record = {};
         //  parse data from APIs
         if (event.body) {
-            var_method = 'API';
             logger.info('API CALL');
-            var_record = [...var_record, ...(await JSON.parse(event.body))];
-        }
-
-        //  parse data from SQS
-        if (event.Records !== undefined) {
-            var_method = 'SQS';
-            logger.info('SQS QUEUE');
-            var_record = [...var_record, ...(await JSON.parse(event.Records[0].body))];
+            var_record = [await JSON.parse(event.body)];
         }
 
         console.log(var_record);
 
-        // Loop for each fetch request
-        for (let x = 0; x < var_record.length; x++) {
-            // Structure the message for SQS
-            let message = var_record[x];
+        // Structure the message for SQS
+        let message = var_record;
 
-            // SQS message parameters
-            const params = {
-                MessageBody: JSON.stringify(message),
-                QueueUrl: queueUrl,
-                MessageGroupId: (await uuidv4()),
-                MessageDeduplicationId: (await uuidv4())
-            };
-            console.log(params);
+        // SQS message parameters
+        const params = {
+            MessageBody: JSON.stringify(message),
+            QueueUrl: queueUrl,
+            MessageGroupId: (await uuidv4()),
+            MessageDeduplicationId: (await uuidv4())
+        };
+        console.log(params);
 
-            await sqs.sendMessage(params).promise().then(
-                function (data) {
-                    console.info("data:", data);
-                }
-            );
-            await sleep(100);
+        await sqs.sendMessage(params).promise().then(
+            function (data) {
+                console.info("data:", data);
+            }
+        );
+        await sleep(100);
 
-        }
         return response.success({
             status: 200,
             error: null
