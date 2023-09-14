74ad03375e21d06850928cf5ebd15049f34c9994
Ben Inkster <ben@geekinc.ca>
2023-09-14 17:19:41 -0600

    Add SendGrid integration

--- server/functions/prospects/sqs-process-email-export.js
+++ server/functions/prospects/sqs-process-email-export.js
@@ -1,6 +1,8 @@
 import { json2csv } from 'json-2-csv';
 import { v4 as uuidv4 } from 'uuid';
 import util from "util";
+import sgMail from '@sendgrid/mail';
+import sgClient from '@sendgrid/client';
 const mysql = require('serverless-mysql')({
     library: require('mysql2'),
     config: {
@@ -12,6 +14,12 @@ const mysql = require('serverless-mysql')({
     }
 });
 
+const setupMailClient = async () => {
+    sgMail.setApiKey(process.env.sendgrid_api_key);
+    sgClient.setApiKey(process.env.sendgrid_api_key);
+    sgClient.setDefaultHeader("Content-Type", "application/json");
+}
+
 export async function main(event, context, req) {
     try {
         // Process each message from the event
@@ -19,11 +27,14 @@ export async function main(event, context, req) {
             let data = JSON.parse(record.body);
             console.log(data);
 
+            // Return the data with a customer email to send the results
+            // If no email is found, use a default email, in this case, mine
             let results = await mysql.query(
                 "SELECT " +
-                "   json_data " +
+                "   IFNULL(c.email, 'ben@dynamicsales.com') as email, p.json_data " +
                 " FROM `customer_prospects` as cp " +
-                " INNER JOIN prospects as p ON cp.prospect_id = p.id" +
+                " INNER JOIN prospects as p ON cp.prospect_id = p.id " +
+                " INNER JOIN customers as c ON cp.customer_id = c.id " +
                 " WHERE cp.customer_id = ?" +
                 " and " +
                 " cp.batch_id = ?" +
@@ -37,7 +48,7 @@ export async function main(event, context, req) {
                 ]);
 
             console.log('first result:');
-            console.log(results[0]);
+            console.log(results[0].json_data);
             console.log('total to process:');
             console.log(results.length);
 
@@ -46,8 +57,29 @@ export async function main(event, context, req) {
                 output.push(results[x].json_data);
             }
 
+            // Generate the CSV
             const csv = await json2csv(output);
             console.log(csv);
+
+            // Set up the email service
+            await setupMailClient();
+            const msg = {
+                to: results[0].email,
+                from: 'Dynamic Sales',
+                subject: 'Latest Prospects',
+                text: 'Here are your prospects.',
+                html: '<strong>Here are your prospects.</strong>',
+                attachments: [
+                    {
+                        content: csv,
+                        filename: 'prospects.csv',
+                        type: 'text/csv',
+                        disposition: 'attachment'
+                    },
+                ],
+            };
+            await sgMail.send(msg);
+
         }
     } catch (e) {
         console.log(util.inspect(e, {showHidden: false, depth: null, colors: true, maxArrayLength: 500}));
