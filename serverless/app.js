3660778ac6dfa5c53360ea883e6a56c39f65b7a2
Ben Inkster <ben@geekinc.ca>
2024-01-18 15:15:20 -0700

    Upgrade required change in setup code

--- serverless/app.js
+++ serverless/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const Auth  = require('./lib/auth.lib.js');
 const cors = require('cors');
 
-const publicRoutes = require('./routes/public.routes');
+const publicRoutes = require('./routes/public.routes.js');
 
 const app = express();
 app.use(Auth.authMiddleware);
@@ -13,9 +13,27 @@ app.use(express.json());
 // Route definitions are in the routes folder
 app.use('/api/', publicRoutes);
 
-const server = serverlessExpress.createServer(app);
+let serverlessExpressInstance
+// const server = serverlessExpress({ app });
+
+function asyncTask () {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve('Setup tasks complete'), 1000)  // mock out some async tasks
+    })
+}
+
+async function setup (event, context) {
+    const asyncValue = await asyncTask()
+    console.log(asyncValue)
+    serverlessExpressInstance = serverlessExpress({ app })
+    return serverlessExpressInstance(event, context)
+}
 
 exports.handler = (event, context) => {
     // console.log(`EVENT: ${JSON.stringify(event)}`);
-    serverlessExpress.proxy(server, event, context);
+    // serverlessExpress.proxy(server, event, context);
+
+    if (serverlessExpressInstance) return serverlessExpressInstance(event, context)
+
+    return setup(event, context)
 };
