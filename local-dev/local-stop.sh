2e458e80ff3ebffea794202b4ea429251ab2556f
Ben Inkster <ben@geekinc.ca>
2024-02-05 18:14:10 -0700

    Added serverless-offline-s3 and MinIO docker container

--- local-dev/local-stop.sh
+++ local-dev/local-stop.sh
@@ -1,9 +1,12 @@
 docker stop local-mysql
 docker stop local-elasticmq
 docker stop local-keycloak
+docker stop local-minio
 
 docker rm local-mysql
 docker rm local-elasticmq
 docker rm local-keycloak
+docker rm local-minio
+
 
 
